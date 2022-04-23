eadf751f2fea13579a7934f5c9c44608fe4b2d2a
Akhilesh Varanasi <avaran@attu4.cs.washington.edu>
2022-04-22 18:30:18 -0700

    elasticTest and lassoTest incomplete

--- elasticTest.py
+++ elasticTest.py
@@ -1,14 +1,25 @@
-# load and summarize the housing dataset
+# use automatically configured elastic net algorithm
+from numpy import arange
 from pandas import read_csv
-from matplotlib import pyplot
-# load dataset
+from sklearn.linear_model import ElasticNetCV
+from sklearn.model_selection import RepeatedKFold
+# load the dataset
 url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/housing.csv'
 dataframe = read_csv(url, header=None)
-# summarize shape
-print(dataframe.shape)
-# summarize first few lines
-print(dataframe.head())
-
+data = dataframe.values
+X, y = data[:, :-1], data[:, -1]
+# define model evaluation method
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
 # define model
-model = ElasticNet(alpha=1.0, l1_ratio=0.5)
+ratios = arange(0, 1, 0.01)
+alphas = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0, 100.0]
+model = ElasticNetCV(l1_ratio=ratios, alphas=alphas, cv=cv, n_jobs=-1)
+# fit model
+model.fit(X, y)
 
+# define new data
+row = [0.00632,18.00,2.310,0,0.5380,6.5750,65.20,4.0900,1,296.0,15.30,396.90,4.98]
+# make a prediction
+yhat = model.predict([row])
+# summarize prediction
+print('Predicted: %.3f' % yhat)
