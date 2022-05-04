ebaafbc362d5d321a1afcd61aa4b5c1932c90122
arjun0520 <jagnaniarjun@gmail.com>
2022-05-03 23:10:46 -0700

    ml ready test files changed

--- SQL_files/query2.sql
+++ SQL_files/query2.sql
@@ -1,6 +1,6 @@
 .mode csv
-.output EPA_hb_test.csv
+.output none_habitable_only_ml_ready.csv
 
-SELECT * FROM EPA_PLANETARY_SYSTEMS;
+SELECT * FROM EPA_PLANETARY_SYSTEMS where hb_flag = 0;
 
 .output stdout
